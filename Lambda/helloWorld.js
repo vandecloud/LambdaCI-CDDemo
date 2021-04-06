af7e375e94f4f767cbf71e6f95d2d161fe4d6f97
vandecloud <63160149+vandecloud@users.noreply.github.com>
2021-04-06 08:35:54 -0300

    Update helloWorld.js

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "Hellow Mundo"});
+    return callback(null, { statusCode: 200, body: "Hellow Mundo 2021"});
 };
