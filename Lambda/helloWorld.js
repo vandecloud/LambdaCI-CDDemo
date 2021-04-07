7aacbe49fc30e072f21c74c4e80b755dc7793fc7
p-vande <ext_pavandec@mercadolibre.com>
2021-04-07 15:31:23 -0300

    Toma de evidencia

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "VERSION 1.0" });
+    return callback(null, { statusCode: 200, body: "🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎🍎" });
 };
