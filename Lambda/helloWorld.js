90ba7a8540195bc1a09c1f7a9d8843409f156421
p-vande <ext_pavandec@mercadolibre.com>
2021-04-07 14:00:06 -0300

    Toma de evidencia Abril 7

--- Lambda/helloWorld.js
+++ Lambda/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "Hola desde lambda 17:22" });
+    return callback(null, { statusCode: 200, body: "Hola Meli Abril 7 13:56" });
 };
