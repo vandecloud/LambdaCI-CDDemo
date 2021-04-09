c4d874d9883abb614f2ffbf3f84916ebb0e31a9d
p-vande <ext_pavandec@mercadolibre.com>
2021-04-09 10:51:33 -0300

    Preparando demo

--- Lambda/preTrafficHook.js
+++ Lambda/preTrafficHook.js
@@ -21,7 +21,7 @@ exports.handler = (event, context, callback) => {
         console.log(`Lambda Data ${JSON.stringify(data)}`);
         const response = JSON.parse(data.Payload);
 
-        if (response.body === "🍍🍏🥭🥝🍇🍍🍒🍅🍉🍐🥑🍋🍊🍓🍈🍎🍑🍌") {
+        if (response.body === "🍏🍏🍏🍏🍏🍏") {
             status = "Succeeded";
         };
 
